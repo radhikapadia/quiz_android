dc6b40591b412175168c59561fb9abf52f0d8546
Xa <xa@xa-Vostro-3446>
2016-04-07 14:25:15 +0530

    bug fixes

--- Wordgame/app/src/main/java/com/typingeek/xa/wordgame/Score.java
+++ Wordgame/app/src/main/java/com/typingeek/xa/wordgame/Score.java
@@ -10,11 +10,15 @@ import android.widget.TextView;
 public class Score extends AppCompatActivity {
 
     private Button mrestartButton;
+    private TextView mscoreView;
+    private String mscore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
-
+        mscoreView = (TextView) findViewById(R.id.score_val);
+        mscore = getIntent().getSerializableExtra("score").toString();
+        mscoreView.setText(mscore.toString());
         mrestartButton = (Button) findViewById(R.id.restart_btn);
         mrestartButton.setOnClickListener(new View.OnClickListener() {
             @Override
